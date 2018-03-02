88f15036613fc2c08df8a94f943ae3729a5849d2
Ivo Ivanov <vn.n0t0@gmail.com>
2018-03-01 19:43:18 -0800

    file - add

---
+++ bash_scripting/02.sh
@@ -0,0 +1,21 @@
+# argument
+# option
+# parameter
+# variable
+
+#### Defined variables
+
+# static: VARNAME=value
+# as an argument $1, $2
+# using 'read'
+
+#### Defining variables with 'read'
+
+# bash -x scriptname --> show every line from the script
+
+#### Variables and subshells
+
+# export --> exports variables to subshells
+
+# /etc/profile
+# /etc/bashrc 
